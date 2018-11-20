826b913e09f0bf7c23b5ae66431456868f815852
alphageek9788 <alphageek9788@gmail.com>
2018-11-20 18:09:57 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,66 @@
+import socket
+import sys
+
+#create socket
+
+def socket_create():
+    try:
+        global host
+        global port
+        global s
+        host = ''
+        port = 9999
+        s = socket.socket()
+    except socket.error as msg:
+        print("socket creation error: " + str(msg))
+
+def socket_bind():
+    try:
+        global host
+        global port
+        global s
+        print("Binding socket to port: " + str(port))
+        s.bind((host, port))
+        s.listen(5)
+    except socket.error as msg:
+        print("socket binding error: " + str(msg) + "\n" + "Retrying.....")
+        socket_bind()
+
+#Establish connection
+
+def socket_accept():
+    conn, address = s.accept()
+    print("Connection has been established |" + "IP " + address[0] + " | Port " + str(address[1]))
+    send_commands(conn)
+    conn.close()
+
+# send commands
+
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == 'quit':
+            conn.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024), "utf-8")
+            print(client_response, end="")
+
+def main():
+    socket_create()
+    socket_bind()
+    socket_accept()
+
+main()
+
+
+
+
+
+
+
+
+
+
+
